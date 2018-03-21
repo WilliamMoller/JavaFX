6f503018e4502ea8ac32015b33019083b15b1e54
WilliamMoller <william.moller.te16d@gmail.com>
2018-03-21 11:52:53 +0100

    nu funkar allt

--- src/Uppgifter/launch.java
+++ src/Uppgifter/launch.java
@@ -22,18 +22,19 @@ public class launch extends Application{
 
 		Group root = new Group();
 
-		Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+		Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT,Color.SKYBLUE);
 
 		for (int i = 0; i < 10; i++) {
 
-			Circle cir = new Circle();
-
-			cir.setTranslateX(WINDOW_WIDTH * Math.random());
-			cir.setTranslateY(WINDOW_HEIGHT * Math.random());
-			cir.setFill(Color.color(Math.random(), Math.random(), Math.random(),0.8));
-			root.getChildren().add(cir);
+			studsboll boll = new studsboll();
+			boll.setTranslateX(WINDOW_WIDTH * Math.random());
+			boll.setTranslateY(WINDOW_HEIGHT * Math.random());
+			boll.setFill(Color.color(Math.random(), Math.random(), Math.random(),0.8));
+			root.getChildren().add(boll);
 		}
 
+		studsboll.startGravity();
+		
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
--- src/Uppgifter/studsboll.java
+++ src/Uppgifter/studsboll.java
@@ -7,7 +7,7 @@ import javafx.scene.shape.Circle;
 
 public class studsboll extends Circle{
 
-		private static ArrayList<studsboll> cir = new ArrayList<studsboll>();
+		private static ArrayList<studsboll> bollar = new ArrayList<studsboll>();
 		private static AnimationTimer gravity;
 		private static double g = 0.982/3;
 		
@@ -18,7 +18,7 @@ public class studsboll extends Circle{
 			this.setRadius(Math.random()* 20 + 20);
 			this.speedY = 0;
 			
-			cir.add(this);
+			bollar.add(this);
 		}
 		
 		public static void startGravity() {
@@ -29,20 +29,21 @@ public class studsboll extends Circle{
 					@Override
 					public void handle(long now) {
 						
-					for (studsboll studsboll : cir) {
+					for (studsboll studsboll : bollar) {
 						studsboll.fall();
 						
 					}
 				}		
 			};
 		}
+			gravity.start();
 	}
 
 		public void fall() {
 			
 			this.speedY += g;
 			
-			if (this.getTranslateY() + speedY > app.WINDOW_HEIGHT - this.getRadius()) {
+			if (this.getTranslateY() + speedY > launch.WINDOW_HEIGHT - this.getRadius()) {
 				speedY *= -1;
 			} else {
 				this.setTranslateY(this.getTranslateY() + speedY);
