ae4cc96cad6d78cd1793cc4b52247e7ee56afbb4
WilliamMoller <33953341+WilliamMoller@users.noreply.github.com>
2018-03-22 20:46:32 +0100

    blabla

--- src/Uppgifter/launch.java
+++ src/Uppgifter/launch.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class launch extends Application{
